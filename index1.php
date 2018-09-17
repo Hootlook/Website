4d52b145e9b2723eecfbcc9a2aab0b103e24c625
Hootlook <42444823+Hootlook@users.noreply.github.com>
2018-09-17 09:01:44 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,71 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8" />
+	<title>Home</title>
+	<link rel="stylesheet" href="style.css" />
+	
+	</head>
+		
+	<body>
+
+		
+		<div class="main">
+			
+				<div class="logo">
+				
+				<img src="image/john1.jpg" alt="logo" title="Accueil" width="200px" height="50px">
+				
+			
+		</div>
+		
+		<div class="banniere">
+			<img src= "image/halo.jpg" alt="bannière" title="Halo" width="799px" height="200px" />
+		</div>
+		
+		<div class="menu">
+				
+					
+					<a href="Accueil.html" class="script">Accueil</a>
+					<a href="Page 1.html" class="script">Page 1</a>
+					<a href="Page 2.html" class="script">Page 2</a>
+					<a href="Contact.html" class="script">Contact</a>
+					
+		</div>
+		
+		
+		
+		
+		
+		
+		<div class="corps">
+		<!--<video autoplay loop muted> == $0
+				<source src="video/Dawning_Frontpage.webm" style="position:absolute; top:0; left:0;" type="video/mp4">!-->
+			
+			<div class="Hi">
+			<img src= "image/hope.png" alt= "Pied" title "Halo" width="785px" height="550"/>
+				<div class="nav">
+					<li><a href="Page 3.html" class="script">Page 3</a></li>
+					<li><a href="Page 4.html" class="script">Page 4</a></li>
+				</div>	
+			</div>
+		
+		<div id="container" style="position:relative;">
+			<div id="Hi" style="position:absolute; top:0; left:0;"></div>
+			<div id="nav" style="position:absolute; top:0; left:0;"></div>
+			
+		</div>
+		
+		</div>
+
+	
+		<div class="pied1">
+			<img src= "image/hope.png" alt= "Pied" title "Halo" width="231px" height="140"/>
+			<div class="viewer-3d-overlay" style="opacity: 0.5;"></div>
+		</div>	
+		
+		
+		</div>
+		</body>
+		</html>
+		
